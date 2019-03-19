f0e4777dcb95b03deb3bae488a8be67eca29dd21
Kevin Koech <34930805+kkkoech@users.noreply.github.com>
2019-03-19 00:02:10 -0400

    Update longest_upsequence.py

--- solutions/longest_upsequence.py
+++ solutions/longest_upsequence.py
@@ -1,1 +1,32 @@
 
+#This is a recursive solution 
+
+#returns index of list where it starts to decrease
+def decrease_index(l):
+    if l == []:
+        return 0
+    elif len(l)==1:
+        return 0
+    elif l[1] > l[0]:
+        return 1 + decrease_index(l[1:])
+    else:
+        return 0
+print(decrease_index([1,2,3,0])) #returns 2
+
+def longest_sequence(l):
+    if l == []:
+        return 0 
+    elif len(l) ==1:
+        return 1
+    else: 
+        return max(decrease_index(l[0:3]),decrease_index(l[2:])) + 1 
+        #Because length of the longest upsequence would be decrease_index() + 1
+
+
+print(longest_sequence([4,1,5,2,6,3,2])) #returns 1
+print(longest_sequence([1,2,3])) #returns 3 
+print(longest_sequence([1,2,3,0])) #returns 3
+print(longest_sequence([4,3,2,1])) #returns 1
+print(longest_sequence([1])) #returns 1
+print(longest_sequence([])) #returns 0 
+
