10e4396116fdcc41f0ae1a74bf6354fb30b51787
kkkoech <kkoech@wesleyan.edu>
2019-06-20 23:38:28 -0400

    Initial solution add-binary

---
+++ solutions/add-binary.py
@@ -0,0 +1,39 @@
+'''
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+'''
+#idea: convert binary to decimal. Add. Them convert back to binary
+def add_binary(a,b):
+    #binary to decimal
+    def to_decimal(c):
+        decimal_val = 0
+        for pos in range(len(c)):
+            decimal_val += 2**(len(c)-(pos+1)) * int(c[pos])
+        return decimal_val
+
+    #decimal to binary
+    def to_binary(c):
+        if c == 0:
+            return 0
+        bins = ""
+        while c > 0:    
+            bins = str(c % 2) + bins
+            c = c // 2
+        return bins
+    
+    return to_binary(to_decimal(a) + to_decimal(b))
+
+#tests
+print(add_binary("0","0"))
+print(add_binary("1","1"))
+print(add_binary("111","100"))
